65fbba9f8466b5d6587a2a842b3db0a400d627ab
芮峰云 <rfyiamcool@163.com>
2015-11-10 16:25:33 +0800

    pypi

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+        name = "swift_rpc",
+        version = "1.0",
+        author = "ruifengyun",
+        author_email = "rfyiamcool@163.com",
+        description = "tornado rpc",
+        license = "MIT",
+        keywords = ["tornado","fengyun"],
+        url = "https://github.com/rfyiamcool",
+        packages = find_packages(),
+        long_description = read('README.md'),
+        install_requires=['ConfigParser','redis'],
+        classifiers = [
+             'Development Status :: 2 - Pre-Alpha',
+             'Intended Audience :: Developers',
+             'License :: OSI Approved :: MIT License',
+             'Programming Language :: Python :: 2.7',
+             'Programming Language :: Python :: 3.0',
+             'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+)
+
